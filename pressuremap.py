56f76e3021e8ca2d253ce130b16e039afd2d7b28
VOLTE Arsene <avolte001@pognon.pedago.ipb.fr>
2017-05-16 12:33:37 +0200

    updated pressure map

--- pressuremap.py
+++ pressuremap.py
@@ -9,15 +9,15 @@ size=1000
 pmap=np.zeros([size,size])
 lstep=0.05
 
-
+'''
 from random import randrange
 
-#def intrados(x):
-#    return x*(x-1)*0.5
+def intrados(x):
+    return x*(x-1)*0.5
 
-#def extrados(x):
-#    return x*(1-x)*0.7
-
+def extrados(x):
+    return x*(1-x)*0.7
+'''
 
 fex=[]
 fin=[]
@@ -27,11 +27,21 @@ for i in range(size) :
 hmax=max(fex)
 hmin=min(fin)
 
-def exdos(x) :
-    return extrados(x)
+def exdos(rx) :
+    if rx<0 :
+        rx=0
+    if rx>=size :
+        rx=size-1
+    return fex[rx]
+    #return extrados(x)
 
-def indos(x) :
-    return intrados(x)
+def indos(rx) :
+    if rx<0 :
+        rx=0
+    if rx>=size :
+        rx=size-1
+    return fin[rx]
+    #return intrados(x)
 
 
 nl=int(1/lstep)
@@ -42,10 +52,10 @@ prin=[0 for i in range(nl+1)]
 for i in range(nl+1) :
     lmbda = i*lstep
     prex[i] = length_curve(
-        lambda x: (lmbda*3*hmax) + (1-lmbda)*exdos(x),
+        lambda x: (lmbda*3*hmax) + (1-lmbda)*exdos(int(round(x*size))),
         1/size, 1-(1/size), step=1/size)
     prin[i] = length_curve(
-        lambda x: (lmbda*3*hmin) + (1-lmbda)*indos(x),
+        lambda x: (lmbda*3*hmin) + (1-lmbda)*indos(int(round(x*size))),
         1/size, 1-(1/size), step=1/size)
 
 
@@ -59,10 +69,10 @@ for ry in range(size) :
     for rx in range(size) :
         y = (size-ry - (hmin/(hmin-hmax))*size) * (3*(hmax-hmin)/size)
         x = rx/size
-        if y >= exdos(x) :
-            pmap[ry][rx] = pressex((y - exdos(x)) / (3*hmax + exdos(x)))
-        elif y <= indos(x) :
-            pmap[ry][rx] = pressin((y - indos(x)) / (3*hmin + indos(x)))
+        if y >= exdos(rx) :
+            pmap[ry][rx] = pressex((y - exdos(rx)) / (3*hmax + exdos(rx)))
+        elif y <= indos(rx) :
+            pmap[ry][rx] = pressin((y - indos(rx)) / (3*hmin + indos(rx)))
         else :
             pmap[ry][rx] = pmap[0][0]
 
